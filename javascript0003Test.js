dc9d920dc8668880a57bf3f9e5d2e2156b0c349c
Raj Shimpi <140874562+RajShimpi@users.noreply.github.com>
2026-03-14 11:25:39 +0530

    Change variable 'rows' to constant 'TOTAL_ROWS'

--- javascript0003Test.js
+++ javascript0003Test.js
@@ -1,6 +1,6 @@
-let rows = 6;
+const TOTAL_ROWS = 6;
 
-for (let i = 1; i <= rows; i++) {
+for (let i = 1; i <= TOTAL_ROWS; i++) {
   let row = "";
   for (let j = 1; j <= i; j++) {
     row += ((i + j + 1) % 2) + " ";
